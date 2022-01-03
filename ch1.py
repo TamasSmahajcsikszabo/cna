8ee7c363fcec3a85f8905cb631f48fdb547e7dc6
Tamas@fedora <tsmahajcsiks@avaya.com>
2022-01-03 14:16:27 +0100

    update

---
+++ ch1.py
@@ -0,0 +1,9 @@
+# complex networks
+# not random, not trivial, not a tree
+# emerges in nature/man-made
+# e.g. preferential attachment
+# e.g. transitive closure: two nodes connect, if they themselves are connected,
+# leading to densely interconnected neighbourhoods
+
+# CNA is a generalization of SNA to include non-social networks
+# 1934 - Moreno - sociograms, first CNs
